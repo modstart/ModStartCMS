a6d6bcb43f5b167a29980789a462cb68da330a75
server <server@server.com>
2021-09-12 00:08:31 +0800

    develop

--- module/Cms/View/widget/cmsInfo.blade.php
+++ module/Cms/View/widget/cmsInfo.blade.php
@@ -4,9 +4,6 @@
             您正在使用
             <b>ModStartCMS</b>
             <b>V{{\App\Constant\AppConstant::VERSION}}</b>
-            ，
-            获取最新代码请访问
-            <a href="https://modstart.com" target="_blank">modstart.com</a>
         </div>
     </div>
 </div>
