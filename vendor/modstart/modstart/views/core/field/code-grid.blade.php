a281f44f6805d618139ebe7dff34a0cfdd685c27
server <server@server.com>
2021-09-12 00:07:34 +0800

    develop

--- vendor/modstart/modstart/views/core/field/code-grid.blade.php
+++ vendor/modstart/modstart/views/core/field/code-grid.blade.php
@@ -1,3 +1,3 @@
 @if(!empty($value))
-    <pre style="margin:0;overflow:auto;width:{{$width}};max-height:{{$maxHeight}}">{{$value}}</pre>
+    <pre style="margin:0;overflow:auto;width:{{$width}};max-height:{{$maxHeight}};padding:0;line-height:1.2em;">{{$value}}</pre>
 @endif
