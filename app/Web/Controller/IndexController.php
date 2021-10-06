6b21e35ade8e761ae029575beb2e6d2dc540504c
ModStart <modstart@163.com>
2021-10-07 00:38:45 +0800

    develop

--- app/Web/Controller/IndexController.php
+++ app/Web/Controller/IndexController.php
@@ -4,12 +4,13 @@
 namespace App\Web\Controller;
 
 use Module\Vendor\Installer\Util\InstallerUtil;
+use Module\Vendor\Provider\HomePage\HomePageProvider;
 
 class IndexController extends BaseController
 {
-    public function index(\Module\Cms\Web\Controller\IndexController $cms)
+    public function index()
     {
         InstallerUtil::checkForInstallRedirect();
-        return $cms->index();
+        return HomePageProvider::call(__METHOD__, '\\Module\\Cms\\Web\\Controller\\IndexController@index');
     }
 }
