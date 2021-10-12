160b2fad9aec2f921e499cf1eb0b96d30526b60c
server <server@server.com>
2021-10-13 00:29:29 +0800

    develop

--- app/Constant/AppConstant.php
+++ app/Constant/AppConstant.php
@@ -5,5 +5,5 @@ namespace App\Constant;
 class AppConstant
 {
     const APP = 'cms';
-    const VERSION = '1.4.1';
+    const VERSION = '1.4.2';
 }
---
+++ app/Constant/asdfasdfasdf.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace App\Constant;
+
+
+class asdfasdfasdf
+{
+    hello
+
+}
