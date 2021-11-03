4eda8e30fcbf49e869662db73e2d80941a6dfcc1
ModStart <modstart@163.com>
2021-11-03 21:17:24 +0800

    develop

--- module/Cms/View/pc/cms/index.blade.php
+++ module/Cms/View/pc/cms/index.blade.php
@@ -6,9 +6,9 @@
 
     <div style="background:#FFF;">
         @if(\ModStart\Core\Util\AgentUtil::isMobile())
-            @include('module::Banner.View.pc.public.banner',['position'=>'home','bannerRatio'=>'5-2'])
-        @else
             @include('module::Banner.View.pc.public.banner',['position'=>'home','bannerRatio'=>'3-2'])
+        @else
+            @include('module::Banner.View.pc.public.banner',['position'=>'home','bannerRatio'=>'5-2'])
         @endif
     </div>
 
