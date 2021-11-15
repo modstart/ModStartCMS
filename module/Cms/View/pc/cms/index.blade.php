7bf5cb4f0738843e5b9e87b8445fb9524df8bfef
ModStart <modstart@163.com>
2021-11-15 12:09:41 +0800

    develop

--- module/Cms/View/pc/cms/index.blade.php
+++ module/Cms/View/pc/cms/index.blade.php
@@ -43,9 +43,9 @@
                         @foreach(Cms::paginateCatByUrl('product',1,4,['where'=>['isRecommend'=>true]]) as $record)
                             <div class="col-md-3 col-6">
                                 <div class="item-p">
-                                    <a class="image" href="{{$record['_url']}}" style="padding:0.25rem 0 0 0;">
+                                    <a class="image" href="{{$record['_url']}}">
                                         <div class="cover contain ub-cover-1-1"
-                                             style="width:90%;margin:0 auto;background-image:url({{\ModStart\Core\Assets\AssetsUtil::fix($record['cover'])}});"></div>
+                                             style="margin:0 auto;background-image:url({{\ModStart\Core\Assets\AssetsUtil::fix($record['cover'])}});"></div>
                                     </a>
                                     <a class="title" href="{{$record['_url']}}">{{$record['title']}}</a>
                                 </div>
