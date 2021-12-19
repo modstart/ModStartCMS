b1fb5e501a53ff0a6fab4c8697bdd3b65904a1cb
ModStart <modstart@163.com>
2021-12-19 21:58:28 +0800

    develop

--- module/Nav/Helpers/MNav.php
+++ module/Nav/Helpers/MNav.php
@@ -1,13 +1,15 @@
 <?php
 
-class MNav
-{
-    public static function all($position = 'head')
-    {
-        $records = \Module\Nav\Util\NavUtil::listByPositionWithCache($position);
-        foreach ($records as $i => $v) {
-            $records[$i]['_attr'] = \Module\Nav\Type\NavOpenType::getBlankAttributeFromValue($v);
-        }
-        return $records;
-    }
-}
+\Illuminate\Support\Facades\Log::info('aaa');
+
+//class MNav
+//{
+//    public static function all($position = 'head')
+//    {
+//        $records = \Module\Nav\Util\NavUtil::listByPositionWithCache($position);
+//        foreach ($records as $i => $v) {
+//            $records[$i]['_attr'] = \Module\Nav\Type\NavOpenType::getBlankAttributeFromValue($v);
+//        }
+//        return $records;
+//    }
+//}
