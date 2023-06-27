b70ef50810b1911e027d71f517437a0c8f57768a
ModStart <modstart@163.com>
2023-06-27 08:59:54 +0800

    bugfix: field checkbox null foreach error

--- vendor/modstart/modstart/views/core/field/checkbox-grid.blade.php
+++ vendor/modstart/modstart/views/core/field/checkbox-grid.blade.php
@@ -1,7 +1,11 @@
-@foreach($value as $v)
-    @if(isset($options[$v]))
-        <span class="ub-tag">{{$options[$v]}}</span>
-    @else
-        <span class="ub-tag">{{$v}}</span>
-    @endif
-@endforeach
+@if(!empty($value))
+    @foreach($value as $v)
+        @if(isset($options[$v]))
+            <span class="ub-tag">{{$options[$v]}}</span>
+        @else
+            <span class="ub-tag">{{$v}}</span>
+        @endif
+    @endforeach
+@else
+    <span class="ub-text-muted">-</span>
+@endif
