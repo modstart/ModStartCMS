b388da87aaf60f579cb7a334f7675954690e4626
ModStart <modstart@163.com>
2023-11-02 23:12:39 +0800

    新增：毫秒时间戳；命令执行日志精简

--- module/Vendor/Command/ScheduleRunAllCommand.php
+++ module/Vendor/Command/ScheduleRunAllCommand.php
@@ -6,10 +6,12 @@ namespace Module\Vendor\Command;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
+use ModStart\Core\Util\ArrayUtil;
 use ModStart\Core\Util\FileUtil;
 use ModStart\Core\Util\ShellUtil;
 use ModStart\Core\Util\TimeUtil;
 use ModStart\ModStart;
+use Module\Vendor\Util\CacheUtil;
 
 class ScheduleRunAllCommand extends Command
 {
@@ -19,23 +21,34 @@ class ScheduleRunAllCommand extends Command
     {
         $php = $this->argument('php');
         $dir = $this->argument('dir');
-        $projects = FileUtil::listFiles($dir);
-        $projects = array_filter($projects, function ($p) {
-            return $p['isDir']
-                && @file_exists($p['pathname'] . '/artisan')
-                && @file_exists($p['pathname'] . '/.env')
-                && !Str::startsWith($p['filename'], '_delete.')
-                && @file_exists($p['pathname'] . '/vendor/modstart/modstart-' . ModStart::env());
-        });
-        shuffle($projects);
+        $hash = md5($php . ':' . $dir);
+        $projects = CacheUtil::remember("Vendor:ScheduleRunAll:Projects:" . $hash,
+            3600,
+            function () use ($dir) {
+                $projects = FileUtil::listFiles($dir);
+                $projects = array_filter($projects, function ($p) {
+                    return $p['isDir']
+                        && @file_exists($p['pathname'] . '/artisan')
+                        && @file_exists($p['pathname'] . '/.env')
+                        && !Str::startsWith($p['filename'], '_delete.')
+                        && @file_exists($p['pathname'] . '/vendor/modstart/modstart-' . ModStart::env());
+                });
+                shuffle($projects);
+                return ArrayUtil::keepItemsKeys($projects, ['pathname']);
+            });
         foreach ($projects as $project) {
             $start = TimeUtil::millitime();
             $command = "$php {$project['pathname']}/artisan schedule:run";
-            echo "$command -> ";
-            ShellUtil::run($command);
+            Log::info("Vendor.ScheduleRunAllCommand.Run - {$command}");
+            $result = ShellUtil::run($command, false);
+            $result = str_replace([
+                "\r"
+            ], "", $result);
+            $result = str_replace("\n", " ", $result);
+            $result = str_replace("Running scheduled command: Closure", "√", $result);
+            $result = str_replace("No scheduled commands are ready to run.", "〇", $result);
             $ms = TimeUtil::millitime() - $start;
-            echo "{$ms}ms\n";
-            Log::info("Vendor.ScheduleRunAllCommand - {$project['pathname']} - {$ms}ms");
+            Log::info("Vendor.ScheduleRunAllCommand.Result - {$result} - {$ms}ms");
         }
     }
 }
