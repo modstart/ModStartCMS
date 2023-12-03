bbc60eca732ed0436360fc931304bcdb977ca976
ModStart <modstart@163.com>
2023-12-03 23:48:12 +0800

    优化：内容区块显示优化

--- module/ContentBlock/View/admin/content.blade.php
+++ module/ContentBlock/View/admin/content.blade.php
@@ -1,25 +1,28 @@
-<div class="tw-shadow tw-rounded tw-p-1" style="width:10rem;">
-    <table class="ub-table border mini">
+<div class="tw-shadow tw-rounded tw-p-1" style="width:10rem;overflow:auto;">
+    <table class="ub-table border mini tw-table-fixed">
         <tbody>
         @if($item->type===\Module\ContentBlock\Type\ContentBlockType::BASIC)
             @if(!empty($item->texts))
                 <tr>
-                    <td>文字</td>
+                    <td width="40">文字</td>
                     <td>
                         @foreach($item->texts as $text)
-                            <div>{{$text}}</div>
+                            <div class="tw-truncate">
+                                <i class="iconfont icon-dot-sm"></i>
+                                {{$text}}
+                            </div>
                         @endforeach
                     </td>
                 </tr>
             @endif
             @if(!empty($item->images))
                 <tr>
-                    <td>图片</td>
+                    <td width="40">图片</td>
                     <td>
                         @foreach($item->images as $image)
                             <a href="javascript:;"
                                data-image-preview="{{\ModStart\Core\Assets\AssetsUtil::fix($image)}}">
-                                <img style="max-width:2rem;max-height:2rem;"
+                                <img style="height:1.5rem;"
                                      src="{{\ModStart\Core\Assets\AssetsUtil::fix($image)}}"/>
                             </a>
                         @endforeach
@@ -28,7 +31,7 @@
             @endif
         @elseif($item->type===\Module\ContentBlock\Type\ContentBlockType::IMAGE)
             <tr>
-                <td width="50">图片</td>
+                <td width="40">图片</td>
                 <td>
                     <a href="javascript:;" data-image-preview="{{\ModStart\Core\Assets\AssetsUtil::fix($item->image)}}">
                         <img style="max-width:2rem;max-height:2rem;"
@@ -38,7 +41,7 @@
             </tr>
         @elseif($item->type===\Module\ContentBlock\Type\ContentBlockType::HTML)
             <tr>
-                <td width="50">内容</td>
+                <td width="40">内容</td>
                 <td>
                     <div class="ub-html">
                         {!! $item->content !!}
